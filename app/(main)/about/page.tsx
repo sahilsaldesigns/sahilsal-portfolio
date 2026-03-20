0c8280c530b939e2b708b8c82f2ce014a851f9c6
vaishak101 <nairvaishak101@gmail.com>
2026-03-20 22:04:18 +0530

    fix: add image about = mobile line

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -31,8 +31,20 @@ export default async function AboutPage() {
                 />
               </div>
 
+              {/* LEFT LINE — mobile only */}
+              <div className="block md:hidden absolute left-0 bottom-5 max-w-[100px] w-1/2 overflow-hidden">
+                <div className="bg-reveal-edge bg-reveal-left"></div>
+                <Image
+                  src="uploads/img/about-mob-left-line.svg"
+                  alt="left background line"
+                  width={800}
+                  height={500}
+                  className="w-full"
+                />
+              </div>
+
               {/* RIGHT LINE — mobile only */}
-              <div className="block md:hidden absolute right-0 top-[50px] max-w-[245px] w-1/2 overflow-hidden">
+              <div className="block md:hidden absolute -right-[25px] top-[38%] max-w-[110px] w-1/2 overflow-hidden">
                 <div className="bg-reveal-edge bg-reveal-right"></div>
                 <Image
                   src="uploads/img/about-mob-right-line.svg"
