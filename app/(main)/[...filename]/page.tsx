3cba5c386c7251c4cdde74ddc03e92b9b0abca0d
vaishak101 <nairvaishak101@gmail.com>
2026-04-08 08:10:53 +0530

    fix: refactor about page layout and integrate background lines in main page

--- app/(main)/[...filename]/page.tsx
+++ app/(main)/[...filename]/page.tsx
@@ -1,6 +1,7 @@
 import ClientPage from "./client-page";
 import client from "../../../tina/__generated__/client";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 
 export async function generateStaticParams() {
   try {
@@ -51,6 +52,38 @@ export default async function Page({
   // If Tina returned but the page is missing, return a 404
   if (!result?.data?.page) {
     notFound();
+  }
+
+  if (relativePath === "about.mdx") {
+    return (
+      <section className="relative overflow-hidden">
+        <ClientPage {...result} className="relative z-2" />
+
+        {/* LEFT LINE — desktop only */}
+        <div className="hidden md:block absolute left-0 top-[156px] max-w-[245px] w-1/2 overflow-hidden pointer-events-none">
+          <div className="bg-reveal-edge bg-reveal-left"></div>
+          <Image src="/uploads/img/about-desk-left-line.svg" alt="left background line" width={800} height={500} className="w-full" />
+        </div>
+
+        {/* RIGHT LINE — desktop only */}
+        <div className="hidden md:block absolute top-1/2 right-0 max-w-[270px] w-1/2 overflow-hidden ml-auto pointer-events-none">
+          <div className="bg-reveal-edge bg-reveal-right"></div>
+          <Image src="/uploads/img/about-desk-right-line.svg" alt="right background line" width={800} height={500} className="w-full" />
+        </div>
+
+        {/* LEFT LINE — mobile only */}
+        <div className="block md:hidden absolute left-0 bottom-5 max-w-[60px] w-1/2 overflow-hidden pointer-events-none">
+          <div className="bg-reveal-edge bg-reveal-left"></div>
+          <Image src="/uploads/img/about-mob-left-line.svg" alt="left background line" width={800} height={500} className="w-full" />
+        </div>
+
+        {/* RIGHT LINE — mobile only */}
+        <div className="block md:hidden absolute -right-[25px] top-[38%] max-w-20 w-1/2 overflow-hidden pointer-events-none">
+          <div className="bg-reveal-edge bg-reveal-right"></div>
+          <Image src="/uploads/img/about-mob-right-line.svg" alt="right background line" width={800} height={500} className="w-full" />
+        </div>
+      </section>
+    );
   }
 
   return <ClientPage {...result} />;
